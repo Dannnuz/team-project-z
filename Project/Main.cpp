e9dc39fa1be6e67a5f6bc00426e975e878bee857
Zaremba Daniel <danutzaremba@gmail.com>
2022-11-29 21:17:51 +0200

    Header for main functions(create, drop, display, etc)

---
+++ Project/Main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void CreateTable() 
+{
+
+}
+
+void CreateIndex() 
+{
+
+}
+
+void DropTable()
+{
+
+}
+
+void DropIndex()
+{
+
+}
+
+void DisplayTable()
+{
+
+}
+
+int main()
+{
+	string command;
+	cin >> command;
+
+
+cout << "My project";
+}
---
+++ Project/MyCLass.h
@@ -0,0 +1,11 @@
+#pragma once
+class Myclass {
+	int value;
+};
+
+class Table {
+
+private:
+
+
+};
