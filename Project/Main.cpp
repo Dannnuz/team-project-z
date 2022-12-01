48b83a95f2d333ac53b6ef38793fc2d67c33d8df
Zaremba Daniel <danutzaremba@gmail.com>
2022-12-01 03:32:50 +0200

    Introducing main classes to the header file

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 #include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
-
+#include "MyCLass.h"
 using namespace std;
 
-void CreateTable() 
-{
 
-}
-
-void CreateIndex() 
-{
-
-}
-
-void DropTable()
-{
-
-}
-
-void DropIndex()
-{
-
-}
-
-void DisplayTable()
-{
-
-}
 
 char* lower(char comanda[])
 {
@@ -35,22 +12,39 @@ char* lower(char comanda[])
 	{
 		comanda[i] = tolower(comanda[i]);
 	}
-
+	
 	return comanda;
 }
 
 
-
 int main()
 {
-	const char* ch = " ";
 	char command[100];
 	cin.getline(command, 100);
+
+
 	char* next_token;
-	char* secCommand = strtok_s(command," ",&next_token);
-	cout << secCommand;
-	secCommand = strtok_s(NULL, " ", &next_token);
-	cout << secCommand;
-	secCommand = strtok_s(NULL, " ", &next_token);
-	cout << secCommand;
-} 
+	char* secCommand = strtok_s(command, " ", &next_token);
+
+
+	if (strcmp(lower(secCommand), "create") == 0)
+	{
+		cout << "ok"<<endl;
+		secCommand = strtok_s(NULL," ", &next_token);
+		if (strcmp(lower(secCommand), "table") == 0)
+		{
+			//the table's name
+			secCommand = strtok_s(NULL," ", &next_token);
+			//verify id the table's name is already used
+			if(secCommand!=NULL)
+			cout << secCommand;
+		}
+	}
+
+	
+
+	
+
+}
+
+	
--- Project/MyCLass.h
+++ Project/MyCLass.h
@@ -1,14 +1,106 @@
 #pragma once
 
-class Collumn {
-	
-private:
+class ALLTables {
+public:
+	TABLE** tabele;
+};
+
+class TABLE
+{
+public:
+	char* tableName;
+	COLLUMN* coloana;
 
 };
 
-class Table {
 
-private:
+class COLLUMN
+{
+public:
+	char* colName;
+	int size;
+	char* type;
+	char** elemente;
+	int lines;
+	const int maxlenght=100;
+
+	COLLUMN()
+	{
+		this->size = 0;
+		this->colName = NULL;
+		this->type = NULL;
+		this->lines = 0;
+		
+	}
+
+	void setSize(int size)
+	{
+		this->size = size;
+	}
+
+	void setColName(const char* colName)
+	{
+		colName = new char[strlen(colName)];
+		strcpy_s(this->colName, strlen(colName) + 1, colName);
+    }
+
+	void setType(const char* type)
+	{
+		type = new char[strlen(type)];
+		strcpy_s(this->type, strlen(type) + 1, type);
+	}
+
+	void setElemente(char** elemente, int lines, const char* value)
+	{
+		if (elemente==NULL)
+		{
+			this->lines = lines;
+			
+			elemente = new char*[lines*sizeof(char*)];
+			for (int i = 0; i < lines; i++)
+				elemente[i] = new char(maxlenght * sizeof(char));
+		}
+		for (int i = 0; i < lines; i++)
+		{
+			strcpy_s(elemente[i], strlen(value + 1), value);
+		}
+	}
+
+	void setLines()
+	{
+		this->lines = lines+1;
+
+	}
+
+	~COLLUMN() {
+		delete[] colName;
+		delete[] type;
+
+	}
+
+};
+
+class CreateTable {
+	TABLE tabel;
+
+
+
 	
-	
+};
+
+
+class DropTable {
+
+};
+
+class DisplayTable {
+
+};
+
+class DropIndex {
+
+};
+
+class CreateIndex {
+
 };
