c899414053746eab83b331ef5fe8c16922814fd7
Zaremba Daniel <danutzaremba@gmail.com>
2022-12-08 19:52:15 +0200

    Phase 1 final commit

--- Project/Main.cpp
+++ Project/Main.cpp
@@ -18,19 +18,29 @@ int main()
 
 	ALLTables database;
 	
-
-	//create table tabelnou ((masini,text,20,logan),(clienti,text,10,marcel),(venit_lunar,text,10,pensie)
-	//create table tabelnou2 ((case,text,20,vila),(locatii,text,10,grozavesti),(pret,int,10,30000)
+	//THESE ARE COMMANDS VERIFIED THAT WORK
+	//create table tabelnou ((masini,text,20,logan),(clienti,text,10,marcel),(venit_lunar,text,10,pensie))
+	
+	//create table tabelnou2 ((case,text,20,vila),(locatii,text,10,grozavesti),(pret,int,10,30000))
+	
 	//delete from tabelnou where column_name = clienti
+	
 	//insert into tabelnou (mercedes,danut,salariu)
+	
 	//insert into tabelnou (toyota,ion,bitcoin)
+	
 	//display table tabelnou
+	
 	//update tabelnou set column_name = masini_sport where column_name = masini
+	
 	//create index coloana_cool on tabelnou (masini)
+	
 	//drop index coloana_cool
+	
 	//select masini_sport from tabelnou where id = 1
+	
 	//select all from tabelnou where id = 2
-	// 
+
 	//here is a loop where we can introduce commands an modify the database
 	while (strcmp(command, "quit") != 0)
 	{
@@ -40,11 +50,17 @@ int main()
 	
 
 	
-
-	
+	//HOW IT WORKS
+	//I created 4 classes that stores all the tables(with all their values) in the AllTables class(database)
+	//ALl classes:
+	//ALLTables
+	//TABLE
+	//COLLUMN
+	//ELEMENTE
+	//by compiling the program there is a loop where u insert commands 1 by 1 an therefore modyfy the database variable
 
 	//MENTIONS
-	//The preoject works just fine for commands that are written correctly( I couldn't manage to create 
+	//The project works just fine for commands that are written correctly( I couldn't manage to create 
 	//a function that verify if the input between paranthesis is correct) 
 	//there is still a lot of work to do,but we are working on it
 	//sorry for not being able to complete phase 1 entirely by the deadline
